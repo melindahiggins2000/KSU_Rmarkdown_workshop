af98816c289589a8ad922e76b06440975b5b5870
Melinda Higgins <melinda.higgins@emory.edu>
2019-10-29 17:00:20 -0400

    add initial files

---
+++ RScript.R
@@ -0,0 +1,28 @@
+# import abalone.csv dataset
+# load readr package, use read_csv()
+# function from readr to import data
+library(readr)
+abalone <- read_csv("abalone.csv")
+
+# get a glimpse of the data
+# use glimpse() function from tibble package
+library(tibble)
+glimpse(abalone)
+
+# load dplyr package
+# select a few variables
+# and get summary stats
+library(dplyr)
+abalone %>% 
+  select(length, diameter, height) %>% 
+  summary()
+
+# load ggplot2 package
+# make plot of number of rings by shellWeight
+library(ggplot2)
+ggplot(abalone, aes(shellWeight, rings, color=sex)) +
+  geom_point() +
+  facet_wrap(vars(sex)) +
+  geom_smooth()
+
+
